a4ec9a9166bf9c6baab84533dd2162faaebbdb32
BDjaekwanee <jaekwanee@gmail.com>
2022-07-04 09:33:34 +0900

    Add : 단순 for문 예제

---
+++ if, switch, for, while statement/FlowEx12.java
@@ -0,0 +1,11 @@
+class FlowEx12 {
+	public static void main(String[] args) {
+		for(int i =1; i<=5; i++)
+			System.out.println(i); //i의 모든값을 출력
+		
+		for(int i=1; i<=5; i++)
+			System.out.print(i); //개행 없이 출력
+		
+		System.out.println();
+	}
+}
