866c7f55dfbbbc323ab6f763fcc93a1e0ad1ffa7
BDjaekwanee <jaekwanee@gmail.com>
2022-07-05 13:06:10 +0900

    Add : 렌덤 배열 생성 예제

---
+++ if, switch, for, while statement/FlowEx9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class FlowEx9 {
+	public static void main(String[] args) {
+		char grade = ' ';
+		
+		System.out.print("당신의 점수를 입력하세요. (1~100)>");
+		
+		Scanner scanner = new Scanner(System.in);
+		int score = scanner.nextInt();
+		
+	}
+}
