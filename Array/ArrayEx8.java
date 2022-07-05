e24b80d0b34cad65f5816c8ad8b07b081027cfb1
BDjaekwanee <jaekwanee@gmail.com>
2022-07-05 12:50:32 +0900

    Add : 로또 번호 생성 예제

---
+++ Array/ArrayEx8.java
@@ -0,0 +1,22 @@
+class ArrayEx8 {
+	public static void main (String[] args) {
+		int[] ball = new int[45];
+		
+		for (int i = 0; i < ball.length; i++) {
+			ball[i] = i + 1;
+		}
+		
+		int temp = 0;
+		int j    = 0;
+		
+		for(int i = 0; i < 6; i++) {
+			j = (int)(Math.random() * 45);
+			temp    = ball[i];
+			ball[i] = ball[j];
+			ball[j] = temp;
+		}
+		
+		for(int k = 0; k < 6; k++) 
+			System.out.printf("ball[%d]=%d%n", k, ball[k]);
+	}
+}
