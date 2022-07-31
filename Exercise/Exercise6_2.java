4ffdfb36be4d17eedd3d2a3c4b3e67cd10a68222
BDjaekwanee <jaekwanee@gmail.com>
2022-07-31 17:28:37 +0900

    Feat: Create Chapter6 Exercise2 <생성자 활용 예제>

---
+++ Exercise/Exercise6_2.java
@@ -0,0 +1,28 @@
+class Exercise6_2 {
+	public static void main(String[] args) {
+		SutdaCard2 card1 = new SutdaCard2(3, false);
+		SutdaCard2 card2 = new SutdaCard2();
+		
+		System.out.println(card1.info());
+		System.out.println(card2.info());
+	}
+}
+
+
+class SutdaCard2{
+	int num;
+	boolean isKwang;
+	
+	SutdaCard2(){
+		this(1, true);
+	}
+	SutdaCard2(int num, boolean isKwang){
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	
+	String info() {
+		return num + ( isKwang ? "K":" ");
+	}
+	
+}
